540dd87dcd17960b0f0eec8503a142aa99642a42
Denys Borysiuk <denisborisiuk@gmail.com>
2023-08-25 23:20:19 +0300

    add features

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 import gulp from 'gulp';
 import { path } from './gulp/config/path.js';
-// import { plugins } from './gulp/config/plugins.js';
+
+import { plugins } from './gulp/config/plugins.js';
 
 global.app = {
   //   isBuild: process.argv.includes('--build'),
   //   isDev: !process.argv.includes('--build'),
   path: path,
   gulp: gulp,
-  //   plugins: plugins,
+  plugins: plugins,
 };
 
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
-// import { html } from './gulp/tasks/html.js';
+import { html } from './gulp/tasks/html.js';
 // import { server } from './gulp/tasks/server.js';
 // import { scss } from './gulp/tasks/scss.js';
 // import { js } from './gulp/tasks/js.js';
@@ -24,7 +25,7 @@ import { reset } from './gulp/tasks/reset.js';
 
 function watcher() {
   gulp.watch(path.watch.files, copy);
-  //   gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
+  gulp.watch(path.watch.html, html); //gulp.series(html, ftp)
   //   gulp.watch(path.watch.scss, scss);
   //   gulp.watch(path.watch.js, js);
   //   gulp.watch(path.watch.images, images);
