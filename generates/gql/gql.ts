e6779c944422c785df11b914434d755fcfbd3f71
Denys Borysiuk <denisborisiuk@gmail.com>
2023-11-11 00:38:44 +0200

    bug fix

--- generates/gql/gql.ts
+++ generates/gql/gql.ts
@@ -2,7 +2,21 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
-const documents = [];
+/**
+ * Map of all GraphQL operations in the project.
+ *
+ * This map has several performance disadvantages:
+ * 1. It is not tree-shakeable, so it will include all operations in the project.
+ * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
+ * 3. It does not support dead code elimination, so it will add unused operations.
+ *
+ * Therefore it is highly recommended to use the babel or swc plugin for production.
+ */
+const documents = {
+    "query getPost($id: ID!) {\n  post(id: $id, idType: SLUG) {\n    id\n    databaseId\n    title\n    uri\n    date\n    featuredImage {\n      node {\n        id\n        sourceUrl\n        altText\n        mediaDetails {\n          width\n          height\n        }\n      }\n    }\n    author {\n      node {\n        name\n        email\n      }\n    }\n  }\n}": types.GetPostDocument,
+    "query getPostsEdges {\n  posts {\n    edges {\n      node {\n        id\n        title\n        date\n        slug\n        featuredImage {\n          node {\n            id\n            sourceUrl\n            altText\n            mediaDetails {\n              width\n              height\n            }\n          }\n        }\n        author {\n          node {\n            name\n            email\n          }\n        }\n      }\n    }\n  }\n}": types.GetPostsEdgesDocument,
+};
+
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  *
@@ -16,6 +30,15 @@ const documents = [];
  * Please regenerate the types.
  */
 export function graphql(source: string): unknown;
+
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query getPost($id: ID!) {\n  post(id: $id, idType: SLUG) {\n    id\n    databaseId\n    title\n    uri\n    date\n    featuredImage {\n      node {\n        id\n        sourceUrl\n        altText\n        mediaDetails {\n          width\n          height\n        }\n      }\n    }\n    author {\n      node {\n        name\n        email\n      }\n    }\n  }\n}"): (typeof documents)["query getPost($id: ID!) {\n  post(id: $id, idType: SLUG) {\n    id\n    databaseId\n    title\n    uri\n    date\n    featuredImage {\n      node {\n        id\n        sourceUrl\n        altText\n        mediaDetails {\n          width\n          height\n        }\n      }\n    }\n    author {\n      node {\n        name\n        email\n      }\n    }\n  }\n}"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "query getPostsEdges {\n  posts {\n    edges {\n      node {\n        id\n        title\n        date\n        slug\n        featuredImage {\n          node {\n            id\n            sourceUrl\n            altText\n            mediaDetails {\n              width\n              height\n            }\n          }\n        }\n        author {\n          node {\n            name\n            email\n          }\n        }\n      }\n    }\n  }\n}"): (typeof documents)["query getPostsEdges {\n  posts {\n    edges {\n      node {\n        id\n        title\n        date\n        slug\n        featuredImage {\n          node {\n            id\n            sourceUrl\n            altText\n            mediaDetails {\n              width\n              height\n            }\n          }\n        }\n        author {\n          node {\n            name\n            email\n          }\n        }\n      }\n    }\n  }\n}"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
