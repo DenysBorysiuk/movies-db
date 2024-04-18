0fa76963cf5258e940c7009f2484e6fac90cb1b4
Denys Borysiuk <denisborisiuk@gmail.com>
2024-04-18 19:04:50 +0300

    add memo

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import {
   Button,
@@ -14,45 +15,47 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 
 import { MovieCardProps } from './types';
 
-export const MovieCard = ({
-  id,
-  title,
-  overview,
-  popularity,
-  enableUserAction,
-  image = '/movie-thumb.png',
-}: MovieCardProps) => {
-  return (
-    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-      <CardMedia component="div" image={image} sx={{ pt: '56.25%' }} />
+export const MovieCard = memo(
+  ({
+    id,
+    title,
+    overview,
+    popularity,
+    enableUserAction,
+    image = '/movie-thumb.png',
+  }: MovieCardProps) => {
+    return (
+      <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <CardMedia component="div" image={image} sx={{ pt: '56.25%' }} />
 
-      <CardContent sx={{ flexGrow: 1 }}>
-        <Typography variant="h5" gutterBottom>
-          {title}
-        </Typography>
+        <CardContent sx={{ flexGrow: 1 }}>
+          <Typography variant="h5" gutterBottom>
+            {title}
+          </Typography>
 
-        <Typography variant="body2" color="text.secondary">
-          {overview}
-        </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {overview}
+          </Typography>
 
-        <Typography variant="button" display="block" mt={2}>
-          {popularity}
-        </Typography>
-      </CardContent>
+          <Typography variant="button" display="block" mt={2}>
+            {popularity}
+          </Typography>
+        </CardContent>
 
-      <CardActions>
-        <Button component={RouterLink} to={`/movies/${id}`} color="secondary">
-          Details
-        </Button>
+        <CardActions>
+          <Button component={RouterLink} to={`/movies/${id}`} color="secondary">
+            Details
+          </Button>
 
-        {enableUserAction && (
-          <Tooltip title="Add to favorites">
-            <IconButton>
-              <FavoriteIcon />
-            </IconButton>
-          </Tooltip>
-        )}
-      </CardActions>
-    </Card>
-  );
-};
+          {enableUserAction && (
+            <Tooltip title="Add to favorites">
+              <IconButton>
+                <FavoriteIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+        </CardActions>
+      </Card>
+    );
+  }
+);
